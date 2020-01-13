b953de7bd4f3e775bba931230344f2fccabad58f
Drobniac <drobniac@gmail.com>
2020-01-13 11:04:03 +0100

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KeepHtmlPipe } from './keep-html.pipe';
 import { RunScriptsDirective } from './run-scripts.directive';
+import { AdminComponent } from './admin/admin.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'admin', component: AdminComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     KeepHtmlPipe,
-    RunScriptsDirective
+    RunScriptsDirective,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
