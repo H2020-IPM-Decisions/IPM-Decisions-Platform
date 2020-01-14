f30bf16491e2b67e83498133da55ba9948b2ae6c
marest94 <stojanovic1994@yahoo.com>
2020-01-14 13:30:30 +0100

    Merge app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,36 @@
-import { UserComponent } from "./user/user.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { EditAccountComponent } from './user/components/account/edit-account/edit-account.component';
+import { AccountComponent } from './user/components/account/account.component';
+import { UserComponent } from './user/user.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { KeepHtmlPipe } from "./keep-html.pipe";
-import { RunScriptsDirective } from "./run-scripts.directive";
-import { AdminComponent } from "./admin/admin.component";
-import { Routes, RouterModule } from "@angular/router";
-import { AccountComponent } from "./user/components/account/account.component";
-import { EditAccountComponent } from "./user/components/account/edit-account/edit-account.component";
-import { ContentManagementSystemComponent } from "./admin/content-management-system/content-management-system.component";
-import { AddUserComponent } from "./admin/add-user/add-user.component";
-import { UserListComponent } from "./admin/user-list/user-list.component";
-import { UserProfileComponent } from "./admin/user-profile/user-profile.component";
-import { DatasetRepositoryComponent } from "./admin/dataset-repository/dataset-repository.component";
-import { ManageMetadataComponent } from "./admin/manage-metadata/manage-metadata.component";
-import { ManageCatalogueComponent } from "./admin/manage-catalogue/manage-catalogue.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { KeepHtmlPipe } from './keep-html.pipe';
+import { RunScriptsDirective } from './run-scripts.directive';
+import { AdminComponent } from './admin/admin.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ContentManagementSystemComponent } from './admin/content-management-system/content-management-system.component';
+import { AddUserComponent } from './admin/add-user/add-user.component';
+import { UserListComponent } from './admin/user-list/user-list.component';
+import { UserProfileComponent } from './admin/user-profile/user-profile.component';
+import { DatasetRepositoryComponent } from './admin/dataset-repository/dataset-repository.component';
+import { ManageMetadataComponent } from './admin/manage-metadata/manage-metadata.component';
+import { ManageCatalogueComponent } from './admin/manage-catalogue/manage-catalogue.component';
 
 const appRoutes: Routes = [
-  { path: "admin", component: AdminComponent },
-  { path: "admin/cms", component: ContentManagementSystemComponent },
-  { path: "admin/add-user", component: AddUserComponent },
-  { path: "admin/user-list", component: UserListComponent },
-  { path: "admin/user-profile", component: UserProfileComponent },
-  { path: "admin/dataset-repo", component: DatasetRepositoryComponent },
-  { path: "admin/manage-metadata", component: ManageMetadataComponent },
-  { path: "admin/manage-catalogue", component: ManageCatalogueComponent },
-  { path: "admin", component: AdminComponent },
-  { path: "user", component: UserComponent },
-  { path: "user/account", component: AccountComponent },
-  { path: "user/account/edit", component: EditAccountComponent }
-];
+  { path: 'admin', component: AdminComponent },
+  { path: 'admin/cms', component: ContentManagementSystemComponent },
+  { path: 'admin/add-user', component: AddUserComponent },
+  { path: 'admin/user-list', component: UserListComponent },
+  { path: 'admin/user-profile', component: UserProfileComponent },
+  { path: 'admin/dataset-repo', component: DatasetRepositoryComponent },
+  { path: 'admin/manage-metadata', component: ManageMetadataComponent },
+  { path: 'admin/manage-catalogue', component: ManageCatalogueComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'user/account', component: AccountComponent },
+  { path: 'user/account/edit', component: EditAccountComponent }
+]
 
 @NgModule({
   declarations: [
@@ -50,8 +49,13 @@ const appRoutes: Routes = [
     AccountComponent,
     EditAccountComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
