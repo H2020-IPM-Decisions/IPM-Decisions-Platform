5c325e7073043c1f72df8c18ad5670f1854534a3
Drobniac <drobniac@gmail.com>
2020-01-15 13:47:53 +0100

    Integrate designer js into the project

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var init: any;
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(
+  ) { }
 
   ngOnInit() {
+    init()
   }
 
 }
