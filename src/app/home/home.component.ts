8a6d760c32b4ac4e48451bf4f8b674bdbd3ea545
Drobniac <drobniac@gmail.com>
2020-02-14 01:04:34 +0100

    Integrate cms data into homepage dss descriptions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,16 +10,28 @@ declare var init: any;
 export class HomeComponent implements OnInit {
 
   bannerUrl = "";
+  dssUseDescription: "";
+  dssEvaluationDescription: "";
+  dssAdaptationDescription: "";
+  dssIntegrationDescription: "";
 
   constructor(
     private cmsService: CMSService
   ) { }
 
   ngOnInit() {
+    let cmsService = this.cmsService;
     init();
-    this.cmsService
-      .getBanner()
+    cmsService.getBanner()
       .then((response: any) => { this.bannerUrl = response.image.path });
+    cmsService.getDSSUse()
+      .then((response: any) => { this.dssUseDescription = response.description });
+    cmsService.getDSSEvaluation()
+      .then((response: any) => { this.dssEvaluationDescription = response.description });
+    cmsService.getDSSAdaptation()
+      .then((response: any) => { this.dssAdaptationDescription = response.description });
+    cmsService.getDSSIntegration()
+      .then((response: any) => { this.dssIntegrationDescription = response.description });
   }
 
 }
