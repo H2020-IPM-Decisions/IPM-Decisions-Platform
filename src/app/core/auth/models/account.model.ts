4adc9b19104a9ebe399d994edce39d17574ba372
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-04-03 11:08:22 +0200

    add account model

---
+++ src/app/core/auth/models/account.model.ts
@@ -0,0 +1,13 @@
+export interface IRegistrationData {
+    firstName: string,
+    lastName: string,
+    userType: string,
+    email: string,
+    phone: string,
+    mobile: string,
+    address: string,
+    postalCode: string,
+    country: string,
+    password: string,
+    confirmPassword: string
+  }
