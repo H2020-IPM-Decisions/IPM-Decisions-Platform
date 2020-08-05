8c19384d937e1181b1be786eecf1aca8d7187b12
Drobniac <drobniac@gmail.com>
2020-08-05 16:36:21 +0200

    Fix Account code

--- src/app/core/auth/models/account.model.ts
+++ src/app/core/auth/models/account.model.ts
@@ -1,13 +1,27 @@
-export interface Account {
-    id: string
-    email: string,
-    token: string,
-    tokenInit: number,
-    tokenExpiration: number,
-    refreshToken: string,
-    claims: string[],
-    roles: string[]
-    //  tokenType: string
-    //  issuer: string,
-    //  audience: string
+export class Account {
+    constructor(
+        public id?: string,
+        public email?: string,
+        public tokenInit?: number,
+        private _tokenExpiration?: number,
+        private _token?: string,
+        public refreshToken?: string,
+        public claims?: string[],
+        public roles?: string[]
+        // public tokenType?: string
+        // private issuer?: string,
+        // private audience?: string
+    ) { }
+    
+    get token() {  
+        if (!this._tokenExpiration || (this.tokenInit > this._tokenExpiration)) {
+            return null;
+        }
+        return this._token;
+    }
+
+    get tokenExpiration() {
+        return this._tokenExpiration;
+    }
+
 }
