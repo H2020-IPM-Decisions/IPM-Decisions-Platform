dadd2e337f493b7b008547c91fdb2d820f970846
Drobniac <drobniac@gmail.com>
2020-09-07 17:35:28 +0200

    Update production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   key: "3fd7616d24a0681333b1532d66bf0f",
-  cmsUrl: 'http://' + window.location.hostname + ':5000/cms',
-  apiUrl: 'http://localhost:5000'
+  cmsUrl: 'https://cms.ipmdecisions.nibio.no',
+  apiUrl: 'https://ipmdecisions.nibio.no',
+  weatherApi: 'https://ipmdecisions.nibio.no/WeatherService/api'
 };
