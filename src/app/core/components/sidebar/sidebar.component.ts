17169801d993d554bcc1d52f54fec24d6bbd1b4f
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-09-19 14:23:55 +0200

    Show sidebar menu based on user role

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '@app/core/auth/services/authentication.service';
 
 @Component({
   selector: 'sidebar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
