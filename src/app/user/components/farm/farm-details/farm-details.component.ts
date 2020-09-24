8cb1b9c7008de0f2af4a3ac7b526a1c79e7e583c
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-09-24 10:27:37 +0200

    Edit farm name and farm location

--- src/app/user/components/farm/farm-details/farm-details.component.ts
+++ src/app/user/components/farm/farm-details/farm-details.component.ts
@@ -1,5 +1,8 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as L from 'leaflet';
+import { throwIfEmpty } from 'rxjs/operators';
 
 @Component({
   selector: 'app-farm-details',
@@ -10,28 +13,57 @@ export class FarmDetailsComponent implements OnInit {
   elements: any = [];
   headElements = ['field', 'type', 'variety', 'sowing_date'];
 
+  cropPestForm: FormGroup;
+  farmForm: FormGroup;
+  canEdit: boolean = false;
+
   private map: L.Map;
   @ViewChild('map', { static: false }) private mapContainer: ElementRef<HTMLElement>;
 
   locations: any = [
-    ["Met. Station_1", 11.8166, 122.0942],
-    ["Met. Station_2", 11.9804, 121.9189],
-    ["Met. Station_3", 10.7202, 122.5621],
-    ["Met. Station_4", 11.3889, 122.6277],
-    ["Met. Station_5", 10.5929, 122.6325]
+    { metStationName: "Met. Station_1", metStationCoords: { lat: 11.8166, lng: 122.0942 } },
+    { metStationName: "Met. Station_2", metStationCoords: { lat: 11.9804, lng: 121.9189 } }
   ];
 
-  constructor() { }
+  farm: any = {
+    id: 123,
+    farmCoords: { lat: 11.8804, lng: 121.9189 },
+    name: "My Farm"
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
 
-    for (let i = 1; i <= 7; i++) {
-      this.elements.push({ field: i, type: 'Crop Type ' + i, variety: 'Crop Variety ' + i, sowing_date: 'Sowing Date ' + i });
-    }   
-    
+    this.formInit();
+    this.farmFormInit();
+
+    // for (let i = 1; i <= 7; i++) {
+    //   this.elements.push({ field: i, type: 'Crop Type ' + i, variety: 'Crop Variety ' + i, sowing_date: 'Sowing Date ' + i });
+    // }
+
   }
 
+  private formInit() {
+    this.cropPestForm = this.fb.group({
+      crop: ['', Validators.required],
+      pest: ['', Validators.required],
+      fieldName: ['', [Validators.required, Validators.minLength(3)]],
+      variety: ['', Validators.required],
+      sowingDate: ['', Validators.required]
+    });
+  }
 
+  private farmFormInit() {
+    this.farmForm = this.fb.group({
+      name: [this.farm.name, Validators.required],
+      // sowingDate: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.cropPestForm.controls;
+  }
   ngAfterViewInit(): void {
     this.initMap();
   }
@@ -57,10 +89,86 @@ export class FarmDetailsComponent implements OnInit {
     // one marker
     // L.marker([51.5, -0.09]).addTo(map).bindPopup("Farm name").openPopup();
 
+    map.fitBounds([
+      [11.5804, 121.5189],
+      [11.9804, 121.9189]
+    ]);
+
+
+    // farm marker
+
+    const farmIcon = L.divIcon({
+      html: '<i class="fas fa-home fa-3x"></i>',
+      iconSize: [20, 20],
+      className: 'myDivIcon'
+    });
+
+    L.marker(this.farm.farmCoords, { icon: farmIcon }).addTo(map).bindPopup(this.farm.name);
+
+    // L.circle(this.farm.farmCoords, {
+    //   color: 'red',
+    //   fillColor: '#f03',
+    //   fillOpacity: 0.5,
+    //   radius: 500
+    // }).addTo(map).bindPopup(this.farm.name);
+
+
     // many markers
     this.locations.forEach((location) => {
-      L.marker([location[1], location[2]]).addTo(map).bindPopup(location[0]).openPopup();
+      L.marker(location.metStationCoords).addTo(map).bindPopup(location.metStationName);
+    });
+
+
+    this.setMarkerLocation(map);
+
+  }
+
+  setMarkerLocation(map: any) {
+    var marker;
+    map.on('click', function (e) {
+      if (marker)
+        map.removeLayer(marker);
+      console.log(e); // e is an event object (MouseEvent in this case)
+      console.log(e.latlng); // e is an event object (MouseEvent in this case)
+      marker = L.marker(e.latlng).addTo(map);
+
+      // { metStationName: "Met. Station_1", metStationCoords: { lat: 11.8166, lng: 122.0942 } },
+      this.farm.metStationCoords = e.latlng;
+      console.log(this.farm.metStationCoords); // e is an event object (MouseEvent in this case)
     });
   }
 
+
+  addToFarm() {
+    if (this.cropPestForm.invalid) { return; }
+
+    const val: CropPest = this.cropPestForm.value;
+
+    if (val.sowingDate) {
+      val.sowingDate = new Date(val.sowingDate).toLocaleDateString('en-GB').split(",")[0];
+    }
+
+    this.elements.push({ field: val.fieldName, type: val.crop, variety: val.variety, sowing_date: val.sowingDate });
+  }
+
+  editFarm(farmId: string) {
+    console.log("farm ", farmId);
+    this.canEdit = !this.canEdit;
+
+    const farmValues = this.farmForm.value;
+
+    this.farm.name = farmValues.name;
+    console.log("va", this.farm.name);
+  }
+
+
+
+}
+
+interface CropPest {
+  crop: string,
+  pest: string,
+  fieldName: string,
+  variety: string,
+  sowingDate: string
 }
