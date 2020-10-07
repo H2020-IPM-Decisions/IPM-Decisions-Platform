4d6930c692065dc850c1e29215ade2cd0b886640
Drobniac <drobniac@gmail.com>
2020-10-07 15:32:06 +0200

    Fix cms service codestyle

--- src/app/shared/services/cms.service.ts
+++ src/app/shared/services/cms.service.ts
@@ -32,111 +32,67 @@ export class CMSService {
 
   getFooter() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/footer?token=${this.key}`).toPromise();
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-
-      // .toPromise();
+      .get(`${this.cmsUrl}/api/singletons/get/footer?token=${this.key}`)
+      .toPromise();
   }
 
   getBanner() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/banner?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/banner?token=${this.key}`)
       .toPromise();
   }
 
   getDSSUse() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/dssuse?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/dssuse?token=${this.key}`)
       .toPromise();
   }
-  
+
   getDSSEvaluation() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/dssevaluation?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/dssevaluation?token=${this.key}`)
       .toPromise();
   }
 
   getDSSAdaptation() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/dssadaptation?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/dssadaptation?token=${this.key}`)
       .toPromise();
   }
 
   getDSSIntegration() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/dssintegration?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/dssintegration?token=${this.key}`)
       .toPromise();
   }
 
   getHomeSlideshow() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/homeslideshow?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/homeslideshow?token=${this.key}`)
       .toPromise();
   }
 
   getHomeArticle1() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/homearticle1?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/homearticle1?token=${this.key}`)
       .toPromise();
   }
 
   getHomeArticle2() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/homearticle2?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/homearticle2?token=${this.key}`)
       .toPromise();
   }
 
   getHomeGrid() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/homegrid?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/homegrid?token=${this.key}`)
       .toPromise();
   }
 
   getNews() {
     return this.http
-      .get(`${this.cmsUrl}/api/singletons/get/news?token=${this.key}`, {
-        // headers: {
-        //   'Cockpit-Token': `${this.key}`
-        // }
-      })
+      .get(`${this.cmsUrl}/api/singletons/get/news?token=${this.key}`)
       .toPromise();
   }
 
