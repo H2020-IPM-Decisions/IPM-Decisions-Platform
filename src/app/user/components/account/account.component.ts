b2a96a0a4cbca4282c11a2d38220c2ff699d92bc
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-10-21 11:36:11 +0200

    Account user details shown with full address

--- src/app/user/components/account/account.component.ts
+++ src/app/user/components/account/account.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "@app/core/auth/services/authentication.service";
+import { UserProfileService } from '@app/shared/services/upr/user-profile.service';
+import { UserProfile } from '@app/shared/models/user-profile.model';
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  selector: "app-account",
+  templateUrl: "./account.component.html",
+  styleUrls: ["./account.component.css"],
 })
 export class AccountComponent implements OnInit {
-
-  constructor() { }
+  user: UserProfile;
+  constructor(
+    private _userProfileService: UserProfileService,
+    public authService: AuthenticationService
+  ) {}
 
   ngOnInit() {
+    this._userProfileService.getUserProfile(true)
+      .subscribe((userFriendly: UserProfile) => {
+        this.user = userFriendly;
+      });
   }
 
 }
