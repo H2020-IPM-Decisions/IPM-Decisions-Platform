a59d457b8dcfd7f2b774194fe5d1cb9a5c4995ca
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-10-28 10:58:13 +0100

    Bug fixed on add new farm

--- src/app/shared/services/upr/farm.service.ts
+++ src/app/shared/services/upr/farm.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { FarmModel } from "@app/shared/models/farm.model";
 import { environment } from "@src/environments/environment";
@@ -13,17 +13,29 @@ export class FarmService {
 
   constructor(private _http: HttpClient) {}
 
-  public createFarm(farm: FarmModel): Observable<FarmModel> {
+  public createFarm(farm: FarmModel): Observable<HttpResponse<FarmModel>> {
     return this._http
-      .post(
-        `${this.apiUrl}/api/upr/farms`,
-        { farm },
-        {
-          headers: {
-            Accept: "application/vnd.h2020ipmdecisions.hateoas+json",
-          },
-        }
-      )
+      .post(`${this.apiUrl}/api/upr/farms`, farm, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/vnd.h2020ipmdecisions.hateoas+json",
+        },
+      })
+      .pipe(
+        catchError((error) => {
+          console.log("error", error);
+          return of(error);
+        })
+      );
+  }
+
+  public getFarms(params?: string[]): Observable<FarmModel[]> {
+    return this._http
+      .get(`${this.apiUrl}/api/upr/farms`, {
+        headers: {
+          Accept: "application/vnd.h2020ipmdecisions.hateoas+json",
+        },
+      })
       .pipe(
         catchError((error) => {
           console.log("error", error);
