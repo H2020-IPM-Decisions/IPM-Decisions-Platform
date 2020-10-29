c107101284170505f0920097af3e026e8c7de95e
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-10-29 16:25:15 +0100

    Demo - crop pest combination

--- src/app/shared/models/farm.model.ts
+++ src/app/shared/models/farm.model.ts
@@ -3,6 +3,6 @@ import { FarmLocation } from "./farm-location.model";
 export interface FarmModel {
   name: string;
   inf1: string;
-  inf2: string;
-  location: FarmLocation;
+  inf2?: string;
+  location?: FarmLocation;
 }
